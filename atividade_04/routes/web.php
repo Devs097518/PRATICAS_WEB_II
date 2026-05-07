ed1e62aada651c9075cc3308379e9cdc71b3eccc
Dayvson Lacerda <dayvsonnlacerda360@gmail.com>
2026-05-07 16:03:17 -0300

    feat: entrega 3/4 da atividade 04 de web II | implementa controllers e views das entidades Author e Publisher

--- atividade_04/routes/web.php
+++ atividade_04/routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\PublisherController;
 
 
 Route::get('/', function () {
@@ -13,3 +15,5 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('categories', CategoryController::class);
+Route::resource('authors', AuthorController::class);
+Route::resource('publishers', PublisherController::class);
